93b93793da2abdf0eb3696506f99d8eaf500d5d0
HelenaW86 <helena.wiklund@rearq.io>
2022-03-23 21:40:03 +0100

    done:)
    Co-authored-by: Ylva Landoff Lindberg <sssylvasss@users.noreply.github.com>

--- src/App.js
+++ src/App.js
@@ -2,11 +2,12 @@ import logo from './logo.svg';
 import './App.css';
 
 import { useTranslation, Trans } from 'react-i18next';
+import { changeLanguage } from 'i18next';
 
 const lngs = {
-  en: { nativeName: 'English' },
-  de: { nativeName: 'Deutsch' },
-  sv: { nativeName: 'Swedish'}
+  en: { nativeName: 'english' },
+  de: { nativeName: 'deutch' },
+  sv: { nativeName: 'swedish'}
 };
 
 function App() {
@@ -16,17 +17,15 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <div>
-          {Object.keys(lngs).map((lng) => (
+
+{Object.keys(lngs).map((lng) => (
             <button key={lng} style={{ fontWeight: i18n.resolvedLanguage === lng ? 'bold' : 'normal' }} type="submit" onClick={() => i18n.changeLanguage(lng)}>
               {lngs[lng].nativeName}
             </button>
           ))}
-        </div>
         <p>
         <Trans i18nKey="description.part1">
-          Edit <code>src/App.js</code> and save to reload.
+        one <code>src/App.js</code>
           </Trans>
         </p>
         <a
@@ -37,9 +36,7 @@ function App() {
         >
           {t('description.part2')}
         </a>
-        <p><Trans i18nKey="description.part3">
-      
-          </Trans></p>
+        <h1>{t('description.part3')}</h1>
       </header>
     </div>
   );
--- src/components/i18n.js
+++ src/components/i18n.js
@@ -2,15 +2,29 @@ import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
 import LanguageDetector from 'i18next-browser-languagedetector';
 
+import translationEn from '../languages/en/translationEn.json'
+import translationSv from '../languages/sv/translationSv.json'
+import translationDe from '../languages/de/translationDe.json'
+
+const resources = {
+  en: {
+    translation: translationEn
+  },
+  sv: {
+    translation: translationSv
+  },
+  de: {
+    translation: translationDe
+  }
+}
+
 i18n
-  // detect user language
-  // learn more: https://github.com/i18next/i18next-browser-languageDetector
+
   .use(LanguageDetector)
-  // pass the i18n instance to react-i18next.
   .use(initReactI18next)
-  // init i18next
-  // for all options read: https://www.i18next.com/overview/configuration-options
   .init({
+    resources,
+    lng: 'sv',
     debug: true,
     fallbackLng: 'en',
     detection: {
@@ -18,38 +32,8 @@ i18n
     cache: ['cookie']
     },
     interpolation: {
-      escapeValue: false, // not needed for react as it escapes by default
+      escapeValue: false,
     },
-    resources: {
-      en: {
-        translation: {
-          // here we will place our translations...
-          description: {
-            part1: 'Edit <1>src/App.js</1> and save to reload.',
-            part2: 'Learn React',
-            part3: 'here we are fully testing!'
-          }
-      }
-    },
-    de: {
-        translation: {
-          description: {
-            part1: 'Ändere <1>src/App.js</1> und speichere um neu zu laden.',
-            part2: 'Lerne React',
-            part3: 'Hier testen wir vollständig'
-          }
-        }
-      },
-      sv: {
-        translation: {
-          description: {
-            part1: 'ändra <1>src/App.js</1> och spara för att ladda om.',
-            part2: 'Lär React',
-            part3: 'här testar vi för fullt!'
-          }
-        }
-      }
-  }
 });
 
 export default i18n;
